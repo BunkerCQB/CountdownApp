51c83d9d8be6a2c73e3e541b68081c54c42a3333
Kieranwebby <79098077+Kieranwebby@users.noreply.github.com>
2024-08-26 20:06:22 +0100

    Updated timer logic and sounds

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import tkinter as tk
 from tkinter.font import Font
 from datetime import datetime
 from pygame import mixer
-
+import serial
+import serial.tools.list_ports
 import threading
 import os
 from tkinter import PhotoImage
@@ -12,7 +13,8 @@ from tkinter import PhotoImage
 mixer.init()
 
 class Config:
-    DURATION = 180
+    DURATION = 180 #Seconds
+    DEAD_BUZZER_TIMER = 5 #Seconds
 
 class CountdownApp:
     def __init__(self, root):
@@ -45,7 +47,8 @@ class CountdownApp:
             "start": {"normal": "green", "active": "#295f29", "disabled": "#808080"},
             "stop": {"normal": "red", "active": "#6f3c3c", "disabled": "#808080"},
             "reset": {"normal": "orange", "active": "#9c6f3c", "disabled": "#808080"},
-            "approve": {"normal": "blue", "active": "#295f29", "disabled": "#808080"}
+            "approve": {"normal": "blue", "active": "#295f29", "disabled": "#808080"},
+            "test_button": {"normal": "black", "active": "#295f29", "disabled": "#808080"}
         }
 
         self.entry_frame = tk.Frame(root, bg="#1c1c1c")
@@ -101,6 +104,11 @@ class CountdownApp:
                                       width=10, height=2, bg=self.button_colors["approve"]["normal"], fg="white",
                                       activeforeground="white", activebackground=self.button_colors["reset"]["active"])
         self.approve_button.grid(row=0, column=3, padx=10, pady=10)
+        
+        self.test_button = tk.Button(self.buttons_frame, text="Dead Button Test", command=self.play_first_warning, font=("Helvetica", 14),
+                                      width=15, height=1, bg=self.button_colors["test_button"]["normal"], fg="white",
+                                      activeforeground="white", activebackground=self.button_colors["test_button"]["active"])
+        self.test_button.grid(row=1, column=1, columnspan=2, padx=10, pady=10)
 
         # Trace Log Listbox with Scrollbar
         self.trace_log = tk.Listbox(root, bg="#1c1c1c", fg="white", height=10, bd=2, relief="solid",
@@ -112,17 +120,19 @@ class CountdownApp:
 
         self.trace_log.config(yscrollcommand=self.scrollbar.set)
         self.scrollbar.config(command=self.trace_log.yview)
-
+        
+        self.port = self.find_arduino_port()
+        
         self.update_timer()
 
     def play_intro(self):
         self.intro_running = True
         self.update_time_left(None)
-        mixer.music.load("./30SecondsStart.mp3")
+        mixer.music.load("./sounds/30SecondsStart.mp3")
         mixer.music.play()
         self.insert_log("Playing intro...", "green")
 
-            # Disabled start, edit and enabled stop
+        # Disabled start, edit and enabled stop
         self.start_button.config(state=tk.DISABLED, bg=self.button_colors["start"]["disabled"])
         self.reset_button.config(state=tk.DISABLED, bg=self.button_colors["reset"]["disabled"])
         self.stop_button.config(state=tk.NORMAL, bg=self.button_colors["stop"]["normal"])
@@ -134,7 +144,7 @@ class CountdownApp:
         self.intro_id = self.root.after(30000, self.start_timer)
 
     def play_approval(self):
-        mixer.music.load("./PointApproved.mp3")
+        mixer.music.load("./sounds/PointApproved.mp3")
         mixer.music.play()
         self.insert_log("Point Approved", "orange")
         
@@ -146,7 +156,8 @@ class CountdownApp:
             self.intro_id = None
 
             self.insert_log("Timer started")
-
+            self.button_active_id = self.root.after(30 * 1000, self.activate_button)
+            
             self.update_timer()
 
     def stop_timer(self):
@@ -183,7 +194,7 @@ class CountdownApp:
             if not self.first_warning_id:
                 self.insert_log("Round Over!", "orange")
 
-            mixer.music.load("./3BeepEnd.mp3")
+            mixer.music.load("./sounds/3BeepEnd.mp3")
             mixer.music.play()
 
     def validate_time_entry(self, new_value):
@@ -220,7 +231,7 @@ class CountdownApp:
                 self.running = False
 
                 # Play the sound file
-                mixer.music.load("./3BeepEnd.mp3")
+                mixer.music.load("./sounds/3BeepEnd.mp3")
                 mixer.music.play()
                 self.insert_log("Playing outro...", "orange")
 
@@ -240,6 +251,9 @@ class CountdownApp:
                 self.intro_id = None
                 self.button_active_id = None
                 self.first_warning_id = None
+
+                self.minutes_entry.config(state="normal")
+                self.seconds_entry.config(state="normal")
 
 
     def get_current_time(self):
@@ -280,7 +294,73 @@ class CountdownApp:
         self.trace_log.insert(tk.END, f"{self.get_current_time()} - {text}")
         self.trace_log.itemconfig(tk.END, {"fg": color})
         self.trace_log.yview(tk.END)  
+    def insert_log(self, text, color = "white"):
+        self.trace_log.insert(tk.END, f"{self.get_current_time()} - {text}")
+        self.trace_log.itemconfig(tk.END, {"fg": color})
+        self.trace_log.yview(tk.END)  
+    
+    def activate_button(self):
+        self.insert_log("Dead Buttons are now activated!", "orange")
+        self.button_active_id = None
+        self.buttons_active = True
 
+    def find_arduino_port(self):
+        ports = serial.tools.list_ports.comports()
+        self.insert_log("Trying to find COM port!")
+        if ports:
+            port = ports[0]  # Select the first available COM port
+            self.insert_log(f"COM port found: {port.device}", "green")
+            self.connect_to_arduino(port.device)
+            return port.device
+    
+        self.insert_log("COM port not found!", "red")
+        self.insert_log("Please try fix & reopen the program", "red")
+        return None
+    
+    def connect_to_arduino(self, port):
+        try:
+            self.serial_connection = serial.Serial(port, 9600)
+            self.insert_log(f"Connected to {port}", "green")
+            threading.Thread(target=self.read_from_serial, daemon=True).start()
+        except Exception as e:
+            self.insert_log(f"Failed to connect: {e}", "red")
+
+    def read_from_serial(self):
+        self.insert_log("Listening for data from serial", "green")
+        while self.serial_connection and self.serial_connection.is_open:
+            try:
+                data = self.serial_connection.readline().strip().decode("utf-8")
+                if data == "Left" or data == "Right":
+                    if self.buttons_active:
+                        if not self.first_warning_id:
+                            self.insert_log(f"{data.upper()} button has been clicked!", "purple")
+                            self.play_first_warning()
+                        else:
+                            self.insert_log(f"{data.upper()} tried click button but it already has been clicked!", "red")
+
+                    else:
+                        self.insert_log(f"{data.upper()} button has been clicked but buttons are not yet active!", "purple")
+
+                    if self.intro_running or self.running:
+                        self.stop_timer()
+                    else:
+                        self.time_left = 180
+                        self.update_timer_label()
+                        self.play_intro()
+
+                
+            except Exception as e:
+                self.insert_log(f"Error: {e}", "red")
+                break
+
+    def play_first_warning(self):
+        self.insert_log("Playing first warning", "orange")
+        self.insert_log("Round will end in " + str(Config.DEAD_BUZZER_TIMER) + " seconds!", "orange")
+
+        mixer.music.load("./sounds/DeadBuzzerEdit.mp3")
+        mixer.music.play()
+
+        self.first_warning_id = self.root.after(Config.DEAD_BUZZER_TIMER*1000, self.stop_timer)
 
 
 if __name__ == "__main__":
